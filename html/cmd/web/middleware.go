fb35ce43a255bb29b14fa2c4bf551a5297647696
Erin Clark <erincodepirate@gmail.com>
2022-01-13 18:08:57 -0800

    Add session management

--- html/cmd/web/middleware.go
+++ html/cmd/web/middleware.go
@@ -7,6 +7,7 @@ import (
 	"github.com/justinas/nosurf"
 )
 
+// test function
 func WriteToConsole(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("Hit the page")
@@ -14,15 +15,21 @@ func WriteToConsole(next http.Handler) http.Handler {
 	})
 }
 
+// add csrf protection
 func NoSurf(next http.Handler) http.Handler {
 	csrfHandler := nosurf.New(next)
 
 	csrfHandler.SetBaseCookie(http.Cookie{
 		HttpOnly: true,
 		Path:     "/",
-		Secure:   false,
+		Secure:   app.InProduction,
 		SameSite: http.SameSiteLaxMode,
 	})
 
 	return csrfHandler
+}
+
+// session load loads session
+func SessionLoad(next http.Handler) http.Handler {
+	return session.LoadAndSave(next)
 }
--- html/go.mod
+++ html/go.mod
@@ -3,7 +3,8 @@ module github.com/erincodepirate/go-webapps/html
 go 1.17
 
 require (
-	github.com/bmizerany/pat v0.0.0-20210406213842-e4b6760bdd6f // indirect
-	github.com/go-chi/chi v1.5.4 // indirect
-	github.com/justinas/nosurf v1.1.1 // indirect
+	github.com/go-chi/chi v1.5.4
+	github.com/justinas/nosurf v1.1.1
 )
+
+require github.com/alexedwards/scs/v2 v2.5.0 // indirect
