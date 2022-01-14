255de67f5821f96b28e2e3b657dfccf4b1239990
Erin Clark <erincodepirate@gmail.com>
2022-01-13 17:18:41 -0800

    Use chi router

--- html/cmd/web/routes.go
+++ html/cmd/web/routes.go
@@ -3,16 +3,24 @@ package main
 import (
 	"net/http"
 
-	"github.com/bmizerany/pat"
+	//"github.com/bmizerany/pat"
 	"github.com/erincodepirate/go-webapps/html/pkg/config"
 	"github.com/erincodepirate/go-webapps/html/pkg/handlers"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 )
 
 func routes(app *config.AppConfig) http.Handler {
-	mux := pat.New()
+	/*mux := pat.New()
 
 	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
-	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))*/
 
+	mux := chi.NewRouter()
+
+	mux.Use(middleware.Recoverer)
+
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
 	return mux
 }
