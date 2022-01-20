725c69b88311976b8b30557d2a73b4d15e65f1de
Erin Clark <erincodepirate@gmail.com>
2022-01-19 17:53:16 -0800

    Add test for render

---
+++ bookings/pkg/render/render_test.go
@@ -0,0 +1,36 @@
+package render
+
+import (
+	"github.com/erincodepirate/go-webapps/bookings/pkg/models"
+	"net/http"
+	"testing"
+)
+
+func TestAddDefaultData(t *testing.T) {
+	var td models.TemplateData
+
+	r, err := getSession()
+	if err != nil {
+		t.Error(err)
+	}
+
+	session.Put(r.Context(), "flash", "123")
+	result := AddDefaultData(&td, r)
+
+	if result.Flash != "123" {
+		t.Error("flash not found in session")
+	}
+}
+
+func getSession() (*http.Request, error) {
+	r, err := http.NewRequest("GET", "/some-url", nil)
+	if err != nil {
+		return nil, err
+	}
+
+	ctx := r.Context()
+	ctx, _ = session.Load(ctx, r.Header.Get("X-Session"))
+	r = r.WithContext(ctx)
+
+	return r, nil
+}
---
+++ bookings/pkg/render/setup_test.go
@@ -0,0 +1,34 @@
+package render
+
+import (
+	"encoding/gob"
+	"github.com/alexedwards/scs/v2"
+	"github.com/erincodepirate/go-webapps/bookings/pkg/config"
+	"github.com/erincodepirate/go-webapps/bookings/pkg/models"
+	"net/http"
+	"os"
+	"testing"
+	"time"
+)
+
+var session *scs.SessionManager
+var testApp config.AppConfig
+
+func TestMain(m *testing.M) {
+	gob.Register(models.Reservation{})
+	// change this to true when in production
+	testApp.InProduction = false
+
+	// set up the session
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = false
+
+	testApp.Session = session
+
+	app = &testApp
+
+	os.Exit(m.Run())
+}
